5494ec7fe2f5f37b9eafed3100086ebae1f5b4c9
srayleighdk <srayleighdk@gmail.com>
2018-08-23 18:26:16 +0700

    add

--- Cac Ky Thuat Tren Mang 2 Chieu/Project2/Source.cpp
+++ Cac Ky Thuat Tren Mang 2 Chieu/Project2/Source.cpp
@@ -87,13 +87,103 @@ void LKYenNgua(int a[][MAX], int dong, int cot)
 		{
 			if (KiemTraYenNgua(a, dong, cot, i, j) == 1)
 			{
-				printf_s("%4d", a[i][j]);
+				printf_s("\n%4d", a[i][j]);
+				printf_s("\nVi tri yen ngua a[%d][%d]", i, j);
 			}
 		}
 	}
 }
-
-
+//Bai tap kiem tra dong tang dan va cot tang dan
+int KiemTraDongTangDan(int a[][MAX], int cot, int chisodong)
+{
+	for (int i = 0; i < cot; i++)
+	{
+		if (a[chisodong][i] < a[chisodong][i - 1])
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+void KiemTraTungDong(int a[][MAX], int dong, int cot)
+{
+	for (int i = 0; i < dong; i++)
+	{
+		if (KiemTraDongTangDan(a, cot, i) == 1)
+		{
+			printf_s("\nDOng %d tang dan", i);
+		}
+		else if( KiemTraDongTangDan(a, cot, i) == 0)
+		{
+			printf_s("\nDong %d khong tang dan", i);
+		}
+	}
+}
+int KiemTraCotTangDan(int a[][MAX], int dong, int chisocot)
+{
+	for (int i = 1; i < dong; i++)
+	{
+		if (a[i][chisocot] < a[i - 1][chisocot])
+		{
+			return 0;
+		}
+	}
+	return 1;
+}
+void KiemTraTungCot(int a[][MAX], int dong, int cot)
+{
+	for (int i = 0; i < cot; i++)
+	{
+		if (KiemTraCotTangDan(a, dong, i) == 1)
+		{
+			printf_s("\nCot %d tang dan", i);
+		}
+		else
+		{
+			printf_s("\nCot %d khong tang dan", i);
+		}
+	}
+}
+//Bai Tap Hoan Vi 2 dong
+void HoanVi(int &a, int &b)
+{
+	int Temp = a; 
+	a = b;
+	b = Temp;
+}
+void HoanVi2Dong(int a[][MAX], int cot, int dong1, int dong2)
+{
+	for (int i = 0; i < cot; i++)
+	{
+		HoanVi(a[dong1][i], a[dong2][i]);
+	}
+}
+//Bai Tap Hoan Vi Cot
+void HoanVi2Cot(int a[][MAX], int dong, int cot1, int cot2)
+{
+	for (int i = 0; i < dong; i++)
+	{
+		HoanVi(a[i][cot1], a[i][cot2]);
+	}
+}
+//Bai Tap Xoa Dong
+void XoaDong(int a[][MAX], int &dong,int cot, int vitridong)
+{
+	for (int i = vitridong; i < dong; i++)
+	{
+		HoanVi2Dong(a, cot, vitridong, (vitridong + 1));
+	}
+	dong--;
+}
+//Bai Tap Xoa Cot
+void XoaCot(int a[][MAX], int dong, int &cot, int vitricot)
+{
+	for (int i = vitricot; i < cot; i++)
+	{
+		HoanVi2Cot(a, dong, i, (i + 1));
+	}
+	cot--;
+}
 int main()
 {
 	int a[MAX][MAX], dong, cot;
@@ -120,7 +210,20 @@ int main()
 	int Tong = TongBien(a, dong, cot);
 	printf_s("\nTong bien la: %d", Tong);
 	LKYenNgua(a, dong, cot);
-
+	KiemTraTungDong(a, cot, dong);
+	KiemTraTungCot(a, dong, cot);
+	HoanVi2Dong(a, cot, 0, 2);
+	printf_s("\nDong sau khi bien doi la!\n");
+	XuatMang(a, dong, cot);
+	HoanVi2Cot(a, dong, 0, 2);
+	printf_s("\nCot sau khi bien doi la!\n");
+	XuatMang(a, dong, cot);
+	XoaDong(a, dong, cot, 0);
+	printf_s("\nMa tran sau khi xoa dong\n");
+	XuatMang(a, dong, cot);
+	XoaCot(a, dong, cot, 0);
+	printf_s("\nMa tran sau khi xoa cot\n");
+	XuatMang(a, dong, cot);
 	_getch();
 	return 0;
 }

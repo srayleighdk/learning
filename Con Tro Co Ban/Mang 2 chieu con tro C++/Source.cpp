dc8c082b125495bda59693205b88eddf13da3f21
srayleighdk <srayleighdk@gmail.com>
2018-09-14 20:01:35 +0700

    add

---
+++ Con Tro Co Ban/Mang 2 chieu con tro C++/Source.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+using namespace std;
+
+void NhapMang(int **a, int dong, int cot)
+{
+	for (int i = 0; i < dong; i++)
+	{
+		for (int j = 0; j < cot; j++)
+		{
+			cout << "\nNhap vao a[" << i << "][" << j << "]";
+			cin >> a[i][j];
+			//Cách 2: cin >> *(a[i] + j);
+			//Cách 3: cin >> *(*(a + i) + j);
+		}
+	}
+}
+
+void XuatMang(int **a, int dong, int cot)
+{
+	for (int i = 0; i < dong; i++)
+	{
+		for (int j = 0; j < cot; j++)
+		{
+			cout << a[i][j] << "     ";
+			//Cách 2: cout << *(a[i] + j);
+			//Cách 3: cout << *(*(a + i) + j);
+		}
+		cout << endl;
+	}
+}
+
+int main()
+{
+	int dong = 2, cot = 3;
+	int **a;
+	a = new int*[dong];
+	for (int i = 0; i < dong; i++)
+	{
+		a[i] = new int[cot];
+	}
+
+	NhapMang(a, dong, cot);
+	XuatMang(a, dong, cot);
+
+	//Giải Phóng
+	for (int i = 0; i < dong; i++)
+	{
+		delete[] a[i];
+	}
+	delete[] a;
+	system("pause");
+	return 0;
+}
---
+++ Con Tro Co Ban/Mang 2 chieu con tro/Source.cpp
@@ -0,0 +1,54 @@
+﻿#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void NhapMang(int **a, int dong, int cot)
+{
+	for (int i = 0; i < dong; i++)
+	{
+		for (int j = 0; j < cot; j++)
+		{
+			printf_s("\nNhap vao a[%d][%d] = ", i, j);
+			scanf_s("%d", &a[i][j]);
+			//Cách 2: scanf_s("%d", a[i] + j);
+			//Cách 3: scanf_s("%d", *(a + i) + j)
+		}
+	}
+}
+
+void XuatMang(int **a, int dong, int cot)
+{
+	for (int i = 0; i < dong; i++)
+	{
+		for (int j = 0; j < cot; j++)
+		{
+			printf_s("%4d", a[i][j]);
+			//Cách 2: printf_s("%4d", *a[i] + j);
+			//Cách 3: printf_s("%4d", *(*(a + i) + j));
+		}
+		printf_s("\n");
+	}
+}
+
+int main()
+{
+	int dong = 2, cot = 3;
+	int **a;
+	a = (int **)malloc(dong * sizeof(int));
+	for (int i = 0; i < dong; i++)
+	{
+		a[i] = (int *)malloc(cot * sizeof(int));
+	}
+
+	NhapMang(a, dong, cot);
+	XuatMang(a, dong, cot);
+
+	//Giải Phóng
+	for (int i = 0; i < dong; i++)
+	{
+		free(a[i]);
+	}
+	free(a);
+	_getch();
+	return 0;
+}
